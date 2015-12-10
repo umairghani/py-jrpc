d5493362c5babbe5bb8bd032f9dbe2ab9d3ffabf
Umair Ghani <umairghani@gmail.com>
2015-12-10 17:47:10 -0600

    adding download_url

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='2.1.1',
     packages=['jrpc', 'jrpc.jrpcClient', 'jrpc.jrpcServer'],
     url='https://github.com/umairghani/py-jrpc',
+    download_url='https://github.com/umairghani/py-jrpc/archive/2.1.1.zip',
     license='The MIT License (MIT)',
     author='Umair Ghani',
     author_email='umairghani@gmail.com',
